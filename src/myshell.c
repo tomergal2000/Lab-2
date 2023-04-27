67b4c548298f77d096005d8e54cdf8586bdd1d7a
Tomer Gal <tomergal2000@gmail.com>
2023-04-27 21:36:58 +0300

    no more memleaks + started proc control

--- src/myshell.c
+++ src/myshell.c
@@ -9,12 +9,127 @@
 #include <sys/wait.h>
 #include "../include/LineParser.h"
 
+#define TERMINATED -1
+#define RUNNING 1
+#define SUSPENDED 0
+    
+
+typedef struct process{
+    cmdLine* cmd;                     /* the parsed command line*/
+    pid_t pid; 		                  /* the process id that is running the command*/
+    int status;                       /* status of the process: RUNNING/SUSPENDED/TERMINATED */
+    struct process *next;	          /* next process in chain */
+} process;
+
+void addProcess(process** process_list, cmdLine* recievedCmd, pid_t recievedPid)
+{
+    struct process *toAdd = (struct process *) malloc(sizeof(struct process));
+    toAdd->cmd = recievedCmd;
+    toAdd->next = NULL;
+    toAdd->pid = recievedPid;
+    toAdd->status = RUNNING;
+    
+    (*process_list)->next = toAdd;
+}
+
+void removeProcess(process **process_list, pid_t toRemove)
+{
+    if (!(*process_list))
+    {
+        return;
+    }
+
+    process *prev = *process_list, *current = (*process_list)->next;
+    if (prev->pid == toRemove)
+    {
+        free(prev);
+        *process_list = current;
+        return;
+    }
+
+    while (current)
+    {
+        if (current->pid == toRemove)
+        {
+            process *next_process = current->next;
+            free(current);
+            prev->next = next_process;
+            break;
+        }
+        prev = current;
+        current = current->next;
+    }
+}
+
+void freeProcessList(process **process_list)
+{
+    process *current = *process_list;
+    while (current)
+    {
+        free(current);
+        current = current->next;
+    }
+}
+
+void changeProcessStatus(process *process_list, pid_t toChange, int new_status)
+{
+    process *current = process_list;
+    while (current)
+    {
+        if (current->pid == toChange)
+        {
+            current->status = new_status;
+        }
+        current = current->next;
+    }
+}
+
+void printProcessList(process** process_list)
+{
+
+}
+
 char containsDebugFlag(int argc, char const *argv[])
 {
     for (int i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], "-d") == 0)
             return 1;
+    }
+    return 0;
+}
+
+int handleSpecialCommands(cmdLine *cmd, process *process_list)
+{
+    if (strcmp(cmd->arguments[0], "cd") == 0)
+    {
+        if (chdir(cmd->arguments[1]) == -1)
+            perror("cd failed");
+        return 1;
+    }
+    if (strcmp(cmd->arguments[0], "suspend") == 0)
+    {
+        int toStop = atoi(cmd->arguments[1]);
+        kill(toStop, SIGTSTP);
+        changeProcessStatus(process_list, toStop, SUSPENDED);
+        usleep(10000);
+        return 1;
+    }
+    if (strcmp(cmd->arguments[0], "wake") == 0)
+    {
+        int toContinue = atoi(cmd->arguments[1]);
+        kill(toContinue, SIGCONT);
+        changeProcessStatus(process_list, toContinue, RUNNING);
+        usleep(10000);
+        return 1;
+    }
+    if (strcmp(cmd->arguments[0], "kill") == 0)
+    {
+        int toTerminate = atoi(cmd->arguments[1]);
+        kill(toTerminate, SIGINT);
+        changeProcessStatus(process_list, toTerminate, TERMINATED);
+        usleep(10000);
+        return 1;
     }
     return 0;
 }
@@ -30,6 +145,7 @@ void execute(cmdLine *pCmdLine)
 
 int main(int argc, char const *argv[])
 {
+    struct process *processList;
     char debug = containsDebugFlag(argc, argv);
 
     while (1)
@@ -50,34 +166,9 @@ int main(int argc, char const *argv[])
         if (debug == 1)
             printf("Executing: %s", input);
 
-        if (strcmp(cmd->arguments[0], "cd") == 0)
+        if (handleSpecialCommands(cmd, processList))
         {
-            if (chdir(cmd->arguments[1]) == -1)
-                perror("cd failed");
-            continue;
-        }
-
-        if (strcmp(cmd->arguments[0], "suspend") == 0)
-        {
-            int toStop = atoi(cmd->arguments[1]);
-            kill(toStop, SIGTSTP);
-            usleep(10000);
-            continue;
-        }
-
-        if (strcmp(cmd->arguments[0], "wake") == 0)
-        {
-            int toContinue = atoi(cmd->arguments[1]);
-            kill(toContinue, SIGCONT);
-            usleep(10000);
-            continue;
-        }
-
-        if (strcmp(cmd->arguments[0], "kill") == 0)
-        {
-            int toTerminate = atoi(cmd->arguments[1]);
-            kill(toTerminate, SIGINT);
-            usleep(10000);
+            freeCmdLines(cmd);
             continue;
         }
 
@@ -88,6 +179,7 @@ int main(int argc, char const *argv[])
             needToPipe = 1;
             if (pipe(fd) == -1)
             {
+                freeCmdLines(cmd);
                 perror("Piping unsuccessful");
                 return 1;
             }
@@ -112,16 +204,21 @@ int main(int argc, char const *argv[])
             {
                 freopen(cmd->outputRedirect, "w", stdout);
             }
+            addProcess(&processList, cmd, pid1);
             execute(cmd);
         }
         else
         { /*Main process*/
             if (cmd->blocking == 1)
             {
-                waitpid(pid1, NULL, WUNTRACED);
+                waitpid(pid1, NULL, 0);
+                removeProcess(&processList, pid1);
             }
             else
+            {
                 usleep(10000);
+            }
+                
 
             if (debug == 1)
             {
@@ -157,6 +254,7 @@ int main(int argc, char const *argv[])
                     {
                         freopen(cmd->next->outputRedirect, "w", stdout);
                     }
+                    addProcess(&processList, cmd->next, pid2);
                     execute(cmd->next);
                 }
             }
